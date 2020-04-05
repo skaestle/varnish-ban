ec12846f75d4530c971c1a297c6d21a18ea3e9d2
Sascha Kaestle <sascha.kaestle@gmail.com>
2020-04-05 21:46:44 +0200

    upgrade to rails 4 due to security concern

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,5 @@ require 'ban_entity_from_varnish'
 
 class Article < ActiveRecord::Base
   include BanEntityFromVarnish
-
-  attr_accessible :lead, :text, :title
   has_and_belongs_to_many :categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,6 @@ require 'ban_entity_from_varnish'
 
 class Category < ActiveRecord::Base
   include BanEntityFromVarnish
-  
-  attr_accessible :name
+
   has_and_belongs_to_many :articles
 end
