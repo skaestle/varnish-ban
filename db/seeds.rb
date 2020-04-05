757e4c03e465333bfc5f9481392a2287efc38652
Sascha Kaestle <sascha.kaestle@gmail.com>
2020-04-05 21:47:30 +0200

    clean up seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-categories = Category.create( [{name: 'science'}, {name: 'politics'}] )
+categories = Category.create([{name: 'science'}, {name: 'politics'}])
+
 categories.each do |cat|
-  5.times do 
+  5.times do
     cat.articles << Article.create( [{title: "title", lead: "lead", text: "text"}] )
   end
 end
 
-categories[1].articles << categories[0].articles.first 
-categories[0].articles << categories[1].articles.first 
-
-categories.each { |cat| cat.save }
+categories[1].articles << categories[0].articles.first
+categories[0].articles << categories[1].articles.last
+categories.map(&:save)
